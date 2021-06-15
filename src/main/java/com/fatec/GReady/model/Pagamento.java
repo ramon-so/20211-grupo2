544e8003d2ecba219280c3fac8901b01159a22fd
Ramon Sousa <Ramon.sousa1997@outlook.com>
2021-06-15 08:03:18 -0300

    salvando

--- src/main/java/com/fatec/GReady/model/Pagamento.java
+++ src/main/java/com/fatec/GReady/model/Pagamento.java
@@ -1,5 +1,21 @@
 package com.fatec.GReady.model;
 
-public class Pagamento {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import com.sun.istack.NotNull;
+
+@Entity
+public class Pagamento {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@NotNull
+	private Matricula matricula;
+	
+	@NotNull
+	private String dataDePagamento;
 }
